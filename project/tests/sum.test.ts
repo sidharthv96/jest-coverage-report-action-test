e5e1926556233a88972ea6e6d2b85ebdc370642c
Sidharth Vinod <sidharthv96@gmail.com>
2021-08-29 00:41:58 +0530

    test

--- project/tests/sum.test.ts
+++ project/tests/sum.test.ts
@@ -7,9 +7,9 @@ describe("sum", () => {
   it("should diff", () => {
     expect(subtr(2, 1)).toBe(1);
   });
-  it("should sum false", () => {
-    expect(sum(1, 2)).toBe(4);
-  });
+  // it("should sum false", () => {
+  //   expect(sum(1, 2)).toBe(4);
+  // });
   // it("should sum false3", () => {
   //   expect(sum(1, 2)).toBe(-1);
   // });
