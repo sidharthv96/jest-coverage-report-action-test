7f4e823cd3367bd8086d1799ee63183c0d6665fd
Sidharth Vinod <sidharthv96@gmail.com>
2021-08-28 19:46:18 +0530

    test: sid action

--- project/index.ts
+++ project/index.ts
@@ -1,16 +1,16 @@
 export const sum = (a: number, b: number) => {
-    if (a > b) {
-        let sum = 0;
+  if (a > b) {
+    let sum = 0;
 
-        for (let c = 0; c < b; c++) {
-            for (let d = 0; d < a; d++) {
-                sum += (c + d * b) / a;
-            }
-        }
-
-        return sum;
+    for (let c = 0; c < b; c++) {
+      for (let d = 0; d < a; d++) {
+        sum += (c + d * b) / a;
+      }
     }
-    return a + b;
+
+    return sum;
+  }
+  return a + b;
 };
 
 export const subtr = (a: number, b: number) => a - b;
